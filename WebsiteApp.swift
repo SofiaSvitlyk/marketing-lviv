d4e9ead37026e53f8d26ee5ee61555dbac4d1352
SofiaSvitlyk <sofasvitlyk03@gmail.com>
2026-03-15 19:39:56 +0200

    upload
    lol

---
+++ WebsiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebsiteApp.swift
+//  Website
+//
+//  Created by macbook on 15.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct WebsiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
